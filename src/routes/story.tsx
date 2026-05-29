71877f51fde00b7b34098c365e45c4c09280d459
Jiten Sony <jitensony@192.168.1.2>
2026-05-29 23:32:50 +0545

    update founder image on story page

--- src/routes/story.tsx
+++ src/routes/story.tsx
@@ -7,7 +7,7 @@ import { ArrowRight, Leaf, Shield, Heart, Sparkles, Droplets, Wind, Star } from 
 
 // Images
 import heroImg from "@/assets/probg.jpg";
-import founderImg from "@/assets/books.png";
+import founderImg from "@/assets/story-image.png";
 import ingredientsImg from "@/assets/ingredients-comp.png";
 import experienceImg from "@/assets/experience.png";
 
